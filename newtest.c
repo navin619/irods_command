7bcdf776e3a2e46acc971b8ae12db9571503f45c
navin619 <nchaganti@email.arizona.edu>
2014-06-16 12:26:08 -0700

    uncommented code

--- newtest.c
+++ newtest.c
@@ -8,15 +8,17 @@
 #include <string.h>
 #include <getopt.h>
 
-void partial_transfer(char *filefrom,char *fileto , char *content)
+void partial_transfer(char *filefrom,char *fileto , int start, int finish)
 {
 FILE *FI, *FO;
     int i1,i2,i3;
     int count, rval, wval,filesize,readCount;
     int ichar;
-	 readCount =atoi(content);
+	 readCount =finish;
     char buf[readCount];
     int seekPos;
+
+
 
    	printf("The partial file in irods is %s and %s",filefrom,fileto);
 	 FI = fopen(filefrom,"r");
@@ -25,6 +27,9 @@ FILE *FI, *FO;
         fprintf(stderr,"can't open input file %s\n",filefrom);
         exit(-2);
     }
+
+       seekPos=start;
+       fseek(FI, seekPos, SEEK_SET);
 
     FO = fopen(fileto,"w");
     if (FO==0) {
@@ -36,7 +41,6 @@ FILE *FI, *FO;
 
        rval = fread(buf, 1, readCount, FI);
        printf("rval=%d\n",rval);
-       //memset(buf, NULL, sizeof(buf));
         printf("%s",buf);
 
 
@@ -70,27 +74,9 @@ void full_transfer(char *filefrom,char *fileto )
         exit(-3);
     }
 
-/*if (argc >= 4) {
-       long seekPos;
-       seekPos=atol(argv[3]);
-       fseek(FO, seekPos, SEEK_CUR);
-    }*/
+
 
     bufsize = sizeof(buf);
-
-   /* if (argc >= 5) {
-      // int i;
-     //  i = atoi(argv[4]);
-       if (i > bufsize) {
-	  perror("buffer size is too big");
-	  exit(-4);
-       }
-       if (i <= 0) {
-	  perror("invalid buffer size");
-	  exit(-5);
-       }
-       bufsize=i;
-    }*/
 
      do {
       memset(buf, 0, sizeof(buf));
@@ -119,87 +105,64 @@ main(int argc, char **argv)
     FILE *FI, *FO;
     int i1,i2,i3,c;
     int count, rval, wval;
-    int pflag=0, fflag=0;
+    int pflag=0, fflag=0,hflag = 0,rflag=0, range=0;
     char buf[1024*100];
+    char *inputfile;
+    char * outputfile;
     int bufsize;
-
-while ((c = getopt(argc, argv, "pf")) != -1)
+    char *from;
+    char *to;
+    char *search = "-";
+while ((c = getopt(argc, argv, "i:o:d:r:")) != -1)
 {
 switch (c) {
              
-   case 'p':
-             pflag = 1;
-             partial_transfer(argv[2], argv[3],argv[4]);
-         	break;
+   case 'i':
+	inputfile=optarg;
+	printf("the input file %s\n",optarg);
+	pflag = 1;
+	break;
 
-    case 'f':
-	     fflag = 1;
-printf("Call the fuction\n");
-		full_transfer(argv[2], argv[3]);
-		
-	       break;
+    case 'o':
+	outputfile=optarg;
+	fflag = 1;
+	printf("the output file %s\n",optarg);
+	break;
+
+     case 'h':
+	hflag = 1;
+	printf("-f <irods :filename> <filename> (for full transfer of a file from irods to local directory )\n");
+	printf("-p <irods :filename> <filename> <number of characters> (for partial transfer of a file from irods to local directory )\n");		
+	break;
+
+case 'r':
+	printf("the range file %s \n",optarg);
+	from=strtok(optarg, search);
+	to =strtok(NULL, search);
+	printf("the range file from %d to %d \n",atoi(from),atoi(to));
+	rflag=1;
+	break;
+
+
+
 
 }
+
+
 }
 
- /*   if (argc < 3) {
-      printf("newtest file-in file-out [seek-position] [buffersize] \n");
-      exit(-1);
-    }
 
-     FI = fopen(argv[1],"r");
-    if (FI==0)
-    {
-        fprintf(stderr,"can't open input file %s\n",argv[1]);
-        exit(-2);
-    }
+if(rflag == 0)
+{
+     full_transfer(inputfile, outputfile);
 
-    FO = fopen(argv[2],"w");
-    if (FO==0) {
-        fprintf(stderr,"can't open output file %s\n",argv[2]);
-        exit(-3);
-    }
-    
-    if (argc >= 4) {
-       long seekPos;
-       seekPos=atol(argv[3]);
-       fseek(FO, seekPos, SEEK_CUR);
-    }
+}
+if(rflag == 1)
+{
+     partial_transfer(inputfile, outputfile,atoi(from),atoi(to));
 
-    bufsize = sizeof(buf);
+}
 
-    if (argc >= 5) {
-       int i;
-       i = atoi(argv[4]);
-       if (i > bufsize) {
-	  perror("buffer size is too big");
-	  exit(-4);
-       }
-       if (i <= 0) {
-	  perror("invalid buffer size");
-	  exit(-5);
-       }
-       bufsize=i;
-    }
-
-     do {
-      memset(buf, 0, sizeof(buf));
-      printf("buffer size=%d\n",bufsize);
-      rval = fread(&buf[0], 1, bufsize, FI);
-      printf("rval=%d\n",rval);
-      if (rval < 0) {
-	perror("read stream message");
-      }
-      if (rval > 20) {
-	wval = fwrite(&buf[0], 1, rval, FO);
-	if (wval < 0) {
-	  perror("fwriting data:");
-	}
-      }
-    } while (rval > 0);
-    fclose(FI);
-    fclose(FO);
-    exit(0);*/
 }
 
 
