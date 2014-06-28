998bda24777b3c72c1dc9a53e1c1f9a4de9c2219
navin619 <nchaganti@email.arizona.edu>
2014-06-27 17:42:36 -0700

    implemented -h and irods:

--- newtest.c
+++ newtest.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
+#include <unistd.h>
 /*
  * filefrom : The input file from irods to  be transfered 
  * fileto   : The output file to be written to in the local drive
@@ -68,7 +69,7 @@ void full_transfer(char *filefrom,char *fileto )
 	char buf[1024*100];
         int bufsize;
 
-   	printf("The full file in irods is %s and %s",filefrom,fileto);
+//   	printf("The full file in irods is %s and %s",filefrom,fileto);
 
     FI = fopen(filefrom,"r");
     if (FI==0)
@@ -89,9 +90,9 @@ void full_transfer(char *filefrom,char *fileto )
 
      do {
       memset(buf, 0, sizeof(buf));
-      printf("buffer size=%d\n",bufsize);
+     // printf("buffer size=%d\n",bufsize);
       rval = fread(&buf[0], 1, bufsize, FI);
-      printf("rval=%d\n",rval);
+      //printf("rval=%d\n",rval);
       if (rval < 0) {
 	perror("read stream message");
       }
@@ -122,20 +123,29 @@ main(int argc, char **argv)
     char *from;
     char *to;
     char *search = "-";
-while ((c = getopt(argc, argv, "i:o:d:r:")) != -1)
+     char *irods = "irods:";
+     int pipefd[2];
+	char *result;
+
+/*char *result = (char*)malloc((strlen(inputfile) + strlen(irods) + 1));
+strcpy(result, irods);
+	strcat(result, inputfile);
+	strcat(irods,inputfile);
+	printf("the input file %s\n",result);*/
+while ((c = getopt(argc, argv, "i:o:r:h")) != -1)
 {
 switch (c) {
    // input file from irods
    case 'i':
+	//printf("we are in the ---i loop");
 	inputfile=optarg;
-	printf("the input file %s\n",optarg);
 	pflag = 1;
 	break;
    // output file in the local drive
     case 'o':
 	outputfile=optarg;
 	fflag = 1;
-	printf("the output file %s\n",optarg);
+//	printf("the output file %s\n",optarg);
 	break;
    // help command
      case 'h':
@@ -145,34 +155,75 @@ switch (c) {
 	break;
    // range to be copied
      case 'r':
-	printf("the range file %s \n",optarg);
+//	printf("the range file %s \n",optarg);
 	from=strtok(optarg, search);
 	to =strtok(NULL, search);
-	printf("the range file from %d to %d \n",atoi(from),atoi(to));
+//	printf("the range file from %d to %d \n",atoi(from),atoi(to));
 	rflag=1;
 	break;
 
-
+}
 
 
 }
 
+if(pflag==1)
+{
+//	printf("we are in the loop");
+	result = (char*)malloc((strlen(inputfile) + strlen(irods) + 1));
+	strcpy(result, irods);
+	strcat(result, inputfile);
+	//strcat(irods,inputfile);
+	//inputfile=
+	//printf("the input file %s\n",result);
+}
 
+
+if(pflag == 0 && hflag == 0)
+{
+	printf("Please enter the input file in format -i <filename>\n");
+	exit(0);
 }
 
 // file to be copied completly
-if(rflag == 0)
+if(rflag == 0 && fflag == 1 && pflag == 1)
 {
-     full_transfer(inputfile, outputfile);
+     full_transfer(result, outputfile);
 
 }
 
 // file to be copied partially
-if(rflag == 1)
+if(rflag == 1 && fflag == 1 && pflag == 1)
 {
-     partial_transfer(inputfile, outputfile,atoi(from),atoi(to));
+      partial_transfer(inputfile, outputfile,atoi(from),atoi(to));
 
 }
+
+if(fflag == 0 && rflag == 0 && pflag == 1 )
+{
+//printf("the files are in pipe and are and ++++\n");
+
+
+ FI = fopen(inputfile,"r");
+    if (FI==0)
+    {
+        fprintf(stderr,"can't open input file %s\n",inputfile);
+        exit(-2);
+    }
+//printf("after opening the file\n");
+int c;
+  if(FI)
+	{
+		while((c=fgetc(FI))!=EOF)
+		 putchar(c);
+    fclose(FI);
+}
+}
+if(hflag = 1 )
+{
+
+}
+
 
 }
 
